37c2feea5acf0e0b6c5256046b480602c3e350b1
Weston Lane <weston.r.lane@gmail.com>
2026-04-12 00:49:44 -0400

    Update ModelLoads.js

--- VR-App/src/ModelLoads.js
+++ VR-App/src/ModelLoads.js
@@ -4,16 +4,19 @@ import * as AssetLoader from './Core/TextureObjectLoader'
  kind of a shitty implementation but we're on a time crunch */
 export async function LoadAssets()
 {
-    const CAR_MODEL = '../public/models/vehicle.glb'
+    // Grab the dynamic base path from Vite
+    const base = import.meta.env.BASE_URL;
+
+    // Append model paths directly to the base (Do NOT include 'public/')
+    const CAR_MODEL = base + 'models/vehicle.glb';
     AssetLoader.AssetCache.models['vehicle'] = 
         await AssetLoader.LoadTextureObjectGLB(CAR_MODEL);
 
-    const PIPE_MODEL = '../public/models/pipe.glb';
+    const PIPE_MODEL = base + 'models/pipe.glb';
     AssetLoader.AssetCache.models['pipe'] = 
         await AssetLoader.LoadTextureObjectGLB(PIPE_MODEL);
 
-    const TARGET_MODEL = '../public/models/Target.glb';
+    const TARGET_MODEL = base + 'models/Target.glb';
     AssetLoader.AssetCache.models['target'] = 
         await AssetLoader.LoadTextureObjectGLB(TARGET_MODEL);
-        
 }
